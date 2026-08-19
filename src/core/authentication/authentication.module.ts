1c14d465599e9caeb457b36bbaa80fd2ece918b2
Omar Vargas <55988968+OmarVargas235@users.noreply.github.com>
2026-08-19 12:27:56 -0500

    feat(NBE-140): implement AuthenticationAccount infrastructure (#112)
    * feat(NBE-140): implement AuthenticationAccount infrastructure
    
    * fix(NBE-140): remove unintended authentication path from tsconfig

--- src/core/authentication/authentication.module.ts
+++ src/core/authentication/authentication.module.ts
@@ -1,4 +1,14 @@
 import { Module } from "@nestjs/common";
 
-@Module({})
+import { AuthenticationAccountRepository } from "./domain/interfaces";
+import { PrismaAuthenticationAccountRepository } from "./infrastructure/repositories";
+
+@Module({
+    providers: [
+        {
+            provide: AuthenticationAccountRepository,
+            useClass: PrismaAuthenticationAccountRepository,
+        },
+    ],
+})
 export class AuthenticationModule {}
---
+++ src/core/authentication/test-doubles/spies/authentication-account.spy.ts
@@ -0,0 +1,9 @@
+import type { AuthenticationAccount } from "../../domain/entities";
+import { AuthenticationAccountRepository } from "../../domain/interfaces";
+
+export class AuthenticationAccountRepositorySpy extends AuthenticationAccountRepository {
+    public create = jest.fn<Promise<void>, [AuthenticationAccount]>();
+    public update = jest.fn<Promise<void>, [AuthenticationAccount]>();
+    public findByUserId = jest.fn<Promise<AuthenticationAccount | null>, [string]>();
+    public findByEmail = jest.fn<Promise<AuthenticationAccount | null>, [string]>();
+}
